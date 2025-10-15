c3afa466973ed501f56ecb4a98b82d504e4d5ec6
juanaltamiranda <juanaltamiranda2003@hotmail.com>
2025-10-15 16:25:12 -0300

    Se realizo desde EstudianteRepository el d, y el e

--- src/main/java/com/example/springbootexample/repository/EstudianteRepository.java
+++ src/main/java/com/example/springbootexample/repository/EstudianteRepository.java
@@ -11,9 +11,11 @@ import java.util.List;
 public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {
 
     //d) recuperar un estudiante, en base a su número de libreta universitaria.
+    @Query("SELECT e FROM Estudiante e WHERE e.nroLibreta =:libreta")
     Estudiante findByNroLibreta(int nroLibreta);
 
     //e) recuperar todos los estudiantes, en base a su género.
+    @Query ("SELECT e FROM Estudiante e WHERE e.genero =:genero")
     List<Estudiante> findByGenero(String genero);
 
 }
